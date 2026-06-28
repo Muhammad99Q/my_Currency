307436d9b3daca5fbb655db5a9e6f797e41e3537
Muhammad Qadr <mahamadqadr97@gmail.com>
2026-06-28 15:54:24 +0300

    Refactor currency conversion logic in index.js

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ function rate(){
 fetch(`https://v6.exchangerate-api.com/v6/c0db9d86a0ffdd9617368f0c/latest/${firstCurency.value}`)
 .then((ans)=>ans.json()).then((data)=>{
   let price= data.conversion_rates[secondCurency.value];
-  price = 150000
 secondInput.value= (price*firstInput.value).toFixed(1);
      document.getElementById("CurrrencyToday").innerText=`1 ${firstCurency.value} = ${price}  ${secondCurency.value}`;
 
