65af4e4b9a42a68205c04fbfe389d06b3276a24e
Muhammad Qadr <mahamadqadr97@gmail.com>
2026-06-28 15:50:33 +0300

    Set fixed conversion price in rate function
    Updated the rate function to set a fixed price for conversion.

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ rate();
 function rate(){
 fetch(`https://v6.exchangerate-api.com/v6/c0db9d86a0ffdd9617368f0c/latest/${firstCurency.value}`)
 .then((ans)=>ans.json()).then((data)=>{
-  const price= data.conversion_rates[secondCurency.value];
+  let price= data.conversion_rates[secondCurency.value];
+  price = 150000
 secondInput.value= (price*firstInput.value).toFixed(1);
      document.getElementById("CurrrencyToday").innerText=`1 ${firstCurency.value} = ${price}  ${secondCurency.value}`;
 
